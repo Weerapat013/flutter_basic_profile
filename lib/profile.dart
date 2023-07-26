be97b9ab49abe65d01009d39c879b09b7750dd69
Weerapat013 <6340011013@psu.ac.th>
2023-07-26 18:34:54 +0700

    change picture of profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,104 +7,99 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Workshop 01: Profile'),
-      ),
-      
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: [
-          buildFeaturePicture(),
-
-          buildNameTitle(),
-
-          buildMail(),
-
-          buildPhoneNumber(),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text('Workshop 01: Profile'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildFeaturePicture(),
+            buildNameTitle(),
+            buildMail(),
+            buildPhoneNumber(),
+          ],
+        ));
   }
 
   Padding buildPhoneNumber() {
     return const Padding(
-          padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
-          child: Row(
-            children: [
-              Icon(Icons.phone,
-                size: 26,
-                color: Colors.cyan,
-              ),
-        
-              SizedBox(
-                width: 10,
-              ),
-        
-              Text('064-0253302',
-                style: TextStyle(
-                  fontFamily: 'Mitr',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.cyan,
-                ),
-              ),
-            ],
+      padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+      child: Row(
+        children: [
+          Icon(
+            Icons.phone,
+            size: 26,
+            color: Colors.cyan,
           ),
-        );
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            '064-0253302',
+            style: TextStyle(
+              fontFamily: 'Mitr',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.cyan,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Padding buildMail() {
     return const Padding(
-          padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
-          child: Row(
-            children: [
-              Icon(Icons.email,
-                size: 26,
-                color: Colors.cyan,
-              ),
-
-              SizedBox(
-                width: 10,
-              ),
-        
-              Text('6340011013@psu.ac.th',
-                style: TextStyle(
-                  fontFamily: 'Mitr',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.cyan,
-                ),
-              ),
-            ],
+      padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+      child: Row(
+        children: [
+          Icon(
+            Icons.email,
+            size: 26,
+            color: Colors.cyan,
           ),
-        );
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            '6340011013@psu.ac.th',
+            style: TextStyle(
+              fontFamily: 'Mitr',
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.cyan,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Padding buildNameTitle() {
     return Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 0, 30),
-          child: Text('Mr.Weerapat Phutthamongkhon',
-            style: GoogleFonts.kanit(
-              textStyle: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
+      padding: const EdgeInsets.fromLTRB(20, 0, 0, 30),
+      child: Text(
+        'Mr.Weerapat Phutthamongkhon',
+        style: GoogleFonts.kanit(
+          textStyle: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
           ),
-        );
+        ),
+      ),
+    );
   }
 
   Padding buildFeaturePicture() {
     return const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Image(
-            image: AssetImage('assets/images/Boo.png'),
-            height: 300,
-            fit: BoxFit.cover,
-          ),
-        );
+      padding: EdgeInsets.all(20.0),
+      child: Image(
+        image: AssetImage('assets/images/profile_1.png'),
+        height: 300,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
